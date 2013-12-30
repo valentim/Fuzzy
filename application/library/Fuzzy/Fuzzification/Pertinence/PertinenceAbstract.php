52dcb86a1f98eca607e7ee971df9595130e499b9
Thiago Valentim <thiago@criawebs.com.br>
2013-12-30 03:52:29 +0100

    adicionei a estrutura de dados para manipular valores e persistencias

--- application/library/Fuzzy/Fuzzification/Pertinence/PertinenceAbstract.php
+++ application/library/Fuzzy/Fuzzification/Pertinence/PertinenceAbstract.php
@@ -5,22 +5,50 @@
  *
  * @author thiagovalentim
  */
-namespace Fuzzy\Fuzzyfication\Pertinence;
+namespace Fuzzy\Fuzzification\Pertinence;
 
 abstract class PertinenceAbstract implements PertinenceInterface
 {
 
     protected $start;
     protected $end;
-    protected $interval = array();
     protected $point;
+    protected $interval = array();
+    protected $set = array();
 
+    public function getPertinenceName() 
+    {
+        $anatomyClass = explode(DIRECTORY_SEPARATOR, get_class($this));
+        
+        return strtolower(end($anatomyClass));      
+    }
+    
+    public function getSet() 
+    {
+        if (empty($this->set)) {
+            for ($i = $this->end; $i > $this->start; $i--) {    
+                $this->addPertinence($i);
+            }
+        }
+        
+        return $this->set;
+    }
+    
     public function setValues(array $values)
     {
         $this->start = (int) current($values);
         end($values);
         $this->end = (int) current($values);
         $this->makeInterval();
+    }
+    
+    protected function addPertinence($value) 
+    {
+        $pertinence = (String) $this->process($value);
+        if (!isset($this->set[$pertinence])) {
+            $this->set[$pertinence] = array();
+        }
+        array_push($this->set[$pertinence], $value);
     }
 
     protected function makeInterval()
@@ -34,6 +62,7 @@ abstract class PertinenceAbstract implements PertinenceInterface
     protected function setIntervalValue($each)
     {
         $this->interval[$this->point] = $this->end;
+        
         while ($this->point) {
             $value = $this->interval[$this->point] - $each;
             $this->point--;
