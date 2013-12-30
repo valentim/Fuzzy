f3f566e22bbcf3418fbacd5345feac3d2f404b52
Thiago Valentim <thiago@criawebs.com.br>
2013-12-30 03:57:44 +0100

    adicionei a função do centro da massa e testes

---
+++ application/library/Fuzzy/Defuzzification/Centroid.php
@@ -0,0 +1,29 @@
+<?php
+namespace Fuzzy\Defuzzification;
+
+class Centroid
+{
+    private $aggregation = array();
+    
+    public function __construct(array $aggregation)
+    {
+        $this->aggregation = $aggregation;
+    }
+    
+    public function calculate()
+    {   
+        $numerator = 0;
+        $denumerator = 0;
+        
+        foreach ($this->aggregation as $pertinence => $values) {
+            $numerator += array_reduce($values, function($v, $w) use($pertinence, &$denumerator) {
+                $v += $w * $pertinence;
+                $denumerator += $pertinence;
+                
+                return $v;
+            });
+        }
+        
+        return round($numerator/$denumerator, 2);
+    }
+}
