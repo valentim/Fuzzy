57ddcc8817616074968537a0152ea8e44d4e63e6
Thiago Valentim <thiago@criawebs.com.br>
2014-01-01 02:14:54 +0100

    exposição de métodos de acesso a dados relevantes

--- application/library/Fuzzy/Fuzzification/FuzzificationInterface.php
+++ application/library/Fuzzy/Fuzzification/FuzzificationInterface.php
@@ -9,4 +9,6 @@ interface FuzzificationInterface
 {
     public function addCollection($type);
     public function addGroup($type, $group, $values);
+    public function getFuzzyCollection();
+    public function getPertinence();
 }
