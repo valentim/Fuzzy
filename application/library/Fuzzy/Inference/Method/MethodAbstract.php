19d31815c87ac0658e59cd7efd7c82f7c39f3885
Thiago Valentim <thiago@criawebs.com.br>
2014-01-01 02:17:00 +0100

    primeira manipulação de rule sets, ainda fraco

--- application/library/Fuzzy/Inference/Method/MethodAbstract.php
+++ application/library/Fuzzy/Inference/Method/MethodAbstract.php
@@ -7,10 +7,41 @@
  */
 namespace Fuzzy\Inference\Method;
 
-abstract class MethodAbstract
+abstract class MethodAbstract implements MethodInterface
 {
+    const PARSEDATA = '/\[(.*?\((.*?)\))\]|[|&:]/';
+    
+    protected $rules = array(
+        'data' => array(),
+    	'max' => array(),
+        'min' => array(),
+        'then' => array()
+    );
+    
     /**
      * 
-     * [[tamanho[medio]]0[sabor[bom]][satisfação[boa]]]
+     * [tamanho(medio)]|[sabor(bom)]:[satisfação(boa)], [tamanho(medio)]|[sabor(bom)]:[satisfação(boa)]
      */
+    public function __construct($linguisticRules) 
+    {
+        $rules = explode(",", $linguisticRules);
+        $this->parseRule($rules);
+    }
+    
+    public function getRules()
+    {
+        return $this->rules;
+    }
+    
+    private function parseRule(array $linguisticRules) 
+    {
+        foreach ($linguisticRules as $rule) {
+            preg_match_all(self::PARSEDATA, $rule, $structures, PREG_SET_ORDER);
+        }
+        
+        $this->rules['data'] = $structures;
+        $this->handlerRules();
+    }
+    
+    abstract protected function handlerRules();
 }
